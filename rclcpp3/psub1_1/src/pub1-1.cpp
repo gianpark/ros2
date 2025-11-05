d45ecd64fb422015928a99d95625f03c955f6205
gianpark <gianpark54@gmail.com>
2025-11-05 20:35:23 +0900

    Change publisher to Int32 and add timer callback
    Updated publisher to use Int32 messages and added a timer callback to publish messages every 100ms.

--- rclcpp3/psub1_1/src/pub1-1.cpp
+++ rclcpp3/psub1_1/src/pub1-1.cpp
@@ -1,27 +1,33 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "rclcpp/time_source.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include <memory>
 #include <chrono>
+#include <functional>
+using namespace std::chrono_literals;
 
-int main(int argc, char *argv[])
+void timer_callback(rclcpp::Node::SharedPtr node,
+                    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr pub)
+{
+    static int count = 0;
+    std_msgs::msg::Int32 msg;
+    msg.data = count++;
+    RCLCPP_INFO(node->get_logger(), "Publish: %d", msg.data);
+    pub->publish(msg);
+}
+
+int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("node_pub1");
+
+    auto node = std::make_shared<rclcpp::Node>("node_pub1_1");
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-    auto mypub = node->create_publisher<std_msgs::msg::String>("topic_pub1", qos_profile);
-    std_msgs::msg::String message;
-    message.data = "Hello world!";
-    rclcpp::WallRate loop_rate(10);
+    auto pub = node->create_publisher<std_msgs::msg::Int32>("topic_1_1", qos_profile);
 
-    while (rclcpp::ok())
-    {
-        RCLCPP_INFO(node->get_logger(), "Publish: %s", message.data.c_str());
-        mypub->publish(message);
-        loop_rate.sleep();
-    }
+    // 100ms마다 콜백 실행
+    std::function<void()> fn = std::bind(timer_callback, node, pub);
+    auto timer = node->create_wall_timer(100ms, fn);
 
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
-
